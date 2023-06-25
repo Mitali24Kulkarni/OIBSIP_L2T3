de8804647a40f476c09b8a5aeab112fdba77f929
Mitali Pramod Kulkarni <79744591+Mitali24Kulkarni@users.noreply.github.com>
2023-06-25 18:56:13 +0530

    Commit of uploaded files

---
+++ todo.js
@@ -0,0 +1,44 @@
+function addTask() {
+    var inputValue = document.getElementById("task").value;
+    if (inputValue === '') {
+        alert("You must write something!");
+        return;
+    }
+
+    var li = document.createElement("li");
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", function() {
+        if (this.checked) {
+            moveTaskToCompleted(this.parentNode);
+        }
+    });
+
+    var label = document.createElement("label");
+    label.appendChild(document.createTextNode(inputValue));
+
+    var deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Remove";
+    deleteButton.addEventListener("click", function() {
+        deleteTask(this.parentNode);
+    });
+
+
+    li.appendChild(label);
+    li.appendChild(checkbox);
+    li.appendChild(deleteButton);
+
+    document.getElementById("myTasks").appendChild(li);
+
+    document.getElementById("task").value = "";
+
+}
+
+function moveTaskToCompleted(taskItem) {
+    var cTasks = document.getElementById("cTasks");
+    cTasks.appendChild(taskItem);
+}
+
+function deleteTask(taskItem) {
+    taskItem.parentNode.removeChild(taskItem);
+}
